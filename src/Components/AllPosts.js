2025feea15d27d0f2a618fc8e0833c9f5679d7d9
florenciasilva <hensgrej@gmail.com>
2019-11-04 12:42:21 -0600

    Add moment dependency

--- src/Components/AllPosts.js
+++ src/Components/AllPosts.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 
 const AllPosts = (props) => {
     return props.posts.map((post, i) => {
         console.log(post)
         return (
-            <p key={i}>{post.content}</p>
+            <div key={i}>
+                <p>{post.content}</p>
+                <time dateTime={post.createdAt}>{moment(post.createdAt).calendar()}</time>
+            </div>
         )
     })
 }
